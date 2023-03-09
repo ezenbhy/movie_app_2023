48e7fd5070e087e2216d343aeea4e7ecb10ccfe5
ezenbhy <ezenbhy@gmail.com>
2023-03-09 10:26:56 +0900

    Home edit

--- src/routes/Home copy.js
+++ src/routes/Home copy.js
@@ -1,30 +1,21 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import Movie from '../components/Movie';
 import '../styles/Home.css';
 
-function Home() {
-  // state = {
-  //   isLoading: true,
-  //   movies: [],
-  // }
+export class Home extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.setState({isLoading:false});
+    // }, 6000);
+    this.getMovies();
+  }
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-
-  // componentDidMount() {
-  //   // setTimeout(() => {
-  //   //   this.setState({isLoading:false});
-  //   // }, 6000);
-  //   this.getMovies();
-  // }
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  const getMovies = async () => {
+  getMovies = async () => {
     const {
       data : {
         data : {
@@ -34,16 +25,14 @@ function Home() {
     } = 
     await axios.get('https://yts-proxy.now.sh/list_movies.json?genre=animation&sort_by=like_count');
     console.log(movies);
-    // this.setState({
-    //   isLoading: false,
-    //   movies,     //movies: movies, //속성:값 이름이 동일하면
-    // })
-    setIsLoading(false);
-    setMovies(movies);
+    this.setState({
+      isLoading: false,
+      movies,     //movies: movies, //속성:값 이름이 동일하면
+    })
   }
 
-  
-    //const {isLoading, movies} = this.state;//구조 분해 할당
+  render() { 
+    const {isLoading, movies} = this.state;//구조 분해 할당
     return (
       
       <section className='container'>
@@ -69,7 +58,7 @@ function Home() {
       </section>
       
     )
- 
+  }
 }
 
 export default Home;
