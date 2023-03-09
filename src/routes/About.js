7991bbb7037168925b6e1d7c2c45522a935d8991
ezenbhy <ezenbhy@gmail.com>
2023-03-09 12:38:51 +0900

    Home edit

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import '../styles/About.css'
+import React from 'react';
+import '../styles/About.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function About() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="about_container">
         <span>“A JavaScript library for building user interfaces”</span> 
